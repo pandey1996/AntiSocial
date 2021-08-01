cbfc4e099399a6965802a561815ac51f54cc6590
Amit Pandey <amitpnd018@gmail.com>
2021-08-01 15:21:43 +0530

    Add a controller and a router for home

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//use express Router
+app.use('/',require('./routes'));
+
+
+
+
+
 app.listen(port, function(err){
     if(err){
         console.log('Error while Listening');
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const router=express.Router();
+const home_controller=require('../controllers/home_controller');
+
+
+console.log("routes loaded");
+
+router.get('/', home_controller.home);
+
+module.exports=router;
