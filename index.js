3d42f83415db1ca2c40834c231a06d8c893c9e3c
Amit Pandey <amitpnd018@gmail.com>
2021-08-07 18:38:57 +0530

    connect-mongo

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db= require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
+const MongoStore=require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -28,7 +29,14 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 10)
-    }
+    },
+    store: new MongoStore({
+        mongooseConnection: db,
+        autoRemove: 'disabled'
+        
+    },function(err){
+        console.log(err || 'connect-mongodb setup ok');
+    })
 }));
 
 app.use(passport.initialize());
