ba79a632de2a3c472835e02ac9786162897364f0
Amit Pandey <amitpnd018@gmail.com>
2021-08-13 21:12:33 +0530

    Post Delete

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,6 +1,6 @@
 const db=require('../config/mongoose');
 const DBcon_post=require('../models/post');
-
+const DBcon_comment=require('../models/comments');
 module.exports.createPosts=function(req,res){
     DBcon_post.create({
         content: req.body.content,
@@ -9,5 +9,20 @@ module.exports.createPosts=function(req,res){
         if(err){ console.log("Error in connecting to DB in posts",err); return res.redirect('back');}
         console.log(prob);
         return res.redirect('back');
+    });
+}
+
+module.exports.destroyPosts=function(req,res){
+    DBcon_post.findById(req.params.id,function(err,post){
+        if(err){ console.log("Error in connecting to DB in posts",err); return res.redirect('back');}
+        if(post.user==req.user.id){
+            post.remove();
+            DBcon_comment.deleteMany({
+                post: req.params.id
+            },function(err){
+                if(err){ console.log("Error in connecting to DB in posts",err); return res.redirect('back');}
+            });
+        }
+        return res.redirect('/');
     });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const router=express.Router();
 const posts_controller=require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication,posts_controller.createPosts);
+router.get('/destroy/:id',passport.checkAuthentication,posts_controller.destroyPosts);
 
 module.exports=router;
